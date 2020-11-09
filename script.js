4013c72da9910d9d1952f3c9c469ad2fcd67a63e
stphncrt <cretestephen@gmail.com>
2020-11-09 12:34:14 +0300

    project completed

--- script.js
+++ script.js
@@ -3,85 +3,91 @@ const planets = [{
         name: 'Mercury',
         orbitalYear: 0.2408467,
         image: './images/mercury.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 2,
         name: "Venus",
         orbitalYear: 0.61519726,
         image: './images/venus.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 3,
         name: "Earth",
         orbitalYear: 1.0,
         image: './images/earth.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 4,
         name: "Mars",
         orbitalYear: 1.88088158,
         image: './images/mars.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 5,
         name: "Jupiter",
         orbitalYear: 11.862615,
         image: './images/jupiter.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 6,
         name: "Saturn",
         orbitalYear: 29.447498,
         image: './images/saturn.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 7,
         name: "Uranus",
         orbitalYear: 84.016846,
         image: './images/uranus.jpg',
-        age: yıl / orbitalYear,
+        // age: yıl / orbitalYear,
     },
     {
         id: 8,
         name: "Neptune",
         orbitalYear: 164.79132,
-        image: './images/neptune.jpg',
-        age: yıl / orbitalYear,
+        image: './images/neptun.jpg',
+        // age: yıl / orbitalYear,
     },
 
 ];
+// age: yıl / orbitalYear,  =>>>> orbital year tanımlanmadı hatası veriyor bu şekilde kulanınca
+
 
 document.querySelector('#btnCalc').addEventListener('click', function() {
-    displayCard(planets);
-
+    displayCard(planets)
 });
 
-let second = document.getElementById('saniye').value.parseInt();
-
-let yıl = second / 31577600;
-
-console.log(yıl);
-
-const cardElement = document.getElementById('cardDisplay');
-// div altında yer alan bir "p" tag ine output atmamız gerekiyor
-
 function displayCard(planet) {
+
+    console.log(planets.orbitalYear)
+    let second = document.getElementById('saniye').value;
+
+    let year = parseInt(second) / 31577600;
+    // let age = yıl /parseInt(planets.orbitalYear) ;   =>>> bu şekilde çağırınca tüm orbital year ları array 
+    // olarak çağırıyor o yüzden bu işlemi for each içinde yapmamız gerek. tek tek yakalayarak aynı işleme 
+    // tabi tutmamız gerek.
+
+
+    const cardElement = document.getElementById('cardDisplay');
+
+
     let cardContent = "";
 
     planet.forEach((element) => {
         cardContent += `<div class="planetCard">
-        <h1 id="planetname" > ${element.name}</h1>
-        <img src="${element.image}" class="planetimg" >
-        <p id="statement" >You are at your ${element.age} if you live on ${element.name} planet.</p>
+    <h1 id="planetname" > ${element.name}</h1>
+    <img src="${element.image}" class="planetimg" >
+    <p id="statement" >You are at your  <span>${ Math.round(parseFloat(year)/parseFloat(element.orbitalYear)*100)/100}</span> if you live on ${element.name} planet.</p>
+</div>`
 
-    </div>`
-
+        //  Math.ronud ()*100/100 ile virgülden sonra 2 basamak alarak yuvarlıyor.
+        console.log(year / element.orbitalYear)
     });
     cardElement.innerHTML = cardContent;
 }
